ebc8cb9ad03fb94691847d430cf0cce3a838ee1a
Rajkumar <rajkumarbestha>
2019-08-26 18:51:52 +0530

    resource ec2 added

--- provider/resource_ec2.go
+++ provider/resource_ec2.go
@@ -1,184 +1,201 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/aws/aws-sdk-go/aws/signer/v4"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"log"
-	"os"
-	"encoding/json"
-	"net/http"
 	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 	"time"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/signer/v4"
+	"github.com/hashicorp/terraform/helper/schema"
 )
 
-func dbaasEC2() *schema.Resource {
-	return &schema.Resource {
+func armadaEc2() *schema.Resource {
+	return &schema.Resource{
 		SchemaVersion: 1,
 		Create:        createEC2,
 		Read:          readEC2,
 		Update:        updateEC2,
 		Delete:        deleteEC2,
-		Schema: map[string]*schema.Schema {
-			"aws_region": &schema.Schema {
+		Schema: map[string]*schema.Schema{
+			"application_code": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"action": &schema.Schema {
+			"environment": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"application_code": &schema.Schema {
+			"request_type_id": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"environment": &schema.Schema {
+			"request_from": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"request_type_id": &schema.Schema {
+			"version": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"request_from": &schema.Schema {
+			"source_server_name": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"version": &schema.Schema {
+			"destination_server": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"source_server_name": &schema.Schema {
+			"source_database_name": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"destination_server": &schema.Schema {
+			"destination_database_name": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"source_database_name": &schema.Schema {
+			"service_account": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"destination_database_name": &schema.Schema {
+			"dbs_dbnameto_restore": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"service_account": &schema.Schema {
+			"server_type": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"dbs_dbnameto_restore": &schema.Schema {
+			"edrive_size": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"server_type": &schema.Schema {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"edrive_size": &schema.Schema {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"service_request_id": &schema.Schema {
+			"service_request_id": &schema.Schema{
 				Type:     schema.TypeString,
 				Optional: true,
 			},
-			"purpose": &schema.Schema {
+			"purpose": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"request_raised_by": &schema.Schema {
+			"request_raised_by": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"db_instance_type": &schema.Schema {
+			"db_instance_type": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"service_request_id_out": &schema.Schema {
-				Type: schema.TypeString,
+			"service_request_id_out": &schema.Schema{
+				Type:     schema.TypeString,
 				Computed: true,
+			},
+			"endpoint": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
 			},
 		}, // Resource Schema End.
 	} // Resource End.
 }
 
-
 func createEC2(d *schema.ResourceData, meta interface{}) error {
-	file, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND, 0666)
-	 if err != nil {
+	// Logging.
+	file, err := os.OpenFile("logs/ec2_create.log", os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
 		log.Fatal(err)
-	 }
+	}
 	defer file.Close()
 	log.SetOutput(file)
-	log.Printf("Terraform apply command given and control is in resource create function \r\n")
-	request := make(map[string]interface{})
-	request["aws_region"] = d.Get("aws_region").(string)
-	request["version"] = d.Get("version").(string)
-	request["environment"] = d.Get("environment").(string)
-	request["action"] = d.Get("action").(string)
-	request["dbInstanceType"] =d.Get("db_instance_type").(string)
-	request["applicationCode"] = d.Get("application_code").(string)
-	request["requestFrom"]=  d.Get("request_from").(string)
-	request["requestTypeID"]=  d.Get("request_type_id").(string)
-	request["requestRaisedBy"]=d.Get("request_raised_by").(string)
-	request["sourceServerName"]= d.Get("source_server_name").(string)
-	request["sourceDatabaseName"]= d.Get("source_database_name").(string)
-	request["destinationServer"] = d.Get("destination_server").(string)
-	request["destinationDatabaseName"] = d.Get("destination_database_name").(string)
-	request["eDriveSize"] = d.Get("edrive_size").(string)
-	request["serviceAccount"] =  d.Get("service_account").(string)
-	request["purpose"] = d.Get("purpose").(string)
-	request["dbsDbnameToRestore"] = d.Get("dbs_dbnameto_restore").(string)
-	log.Printf("Response received from Num Verify API. \r\n")
-	//ec2create := EC2CreateRequest{aws_region, version, environment, action, dbInstanceType, applicationCode, requestFrom, requestRaisedBy, requestTypeID, sourceServerName, sourceDatabaseName, destinationServer,/* eDriveSize,destinationDatabaseName, serviceAccount, purpose, dbsDbnameToRestore*/}
-	ec2createjson, err := json.Marshal(request)
-	log.Printf("Response received from Num Verify API. \r\n")
-  req, err := http.NewRequest("POST", "https://1w3zqo2l9i.execute-api.us-east-1.amazonaws.com/default/python_lambda", bytes.NewBuffer(ec2createjson))
-  req.Header.Set("X-Custom-Header", "myvalue")
-  req.Header.Set("Content-Type", "application/json")
 
-	//log.Println(reflect.TypeOf(v).String())
-	log.Println(d.Id()) 
+	log.Printf("Terraform apply command given and control is in resource create function. \r\n")
+
+	// Make a request object.
+	ec2CreateReq := make(map[string]interface{})
+	ec2CreateReq["version"] = d.Get("version").(string)
+	ec2CreateReq["environment"] = d.Get("environment").(string)
+	ec2CreateReq["dbInstancestype"] = d.Get("db_instance_type").(string)
+	ec2CreateReq["applicationCode"] = d.Get("application_code").(string)
+	ec2CreateReq["requestFrom"] = d.Get("request_from").(string)
+	ec2CreateReq["requestTypeID"] = d.Get("request_type_id").(string)
+	ec2CreateReq["requestRaisedBy"] = d.Get("request_raised_by").(string)
+	ec2CreateReq["sourceServerName"] = d.Get("source_server_name").(string)
+	ec2CreateReq["sourceDatabaseName"] = d.Get("source_database_name").(string)
+	ec2CreateReq["destinationServer"] = d.Get("destination_server").(string)
+	ec2CreateReq["destinationDatabaseName"] = d.Get("destination_database_name").(string)
+	ec2CreateReq["eDriveSize"] = d.Get("edrive_size").(string)
+	ec2CreateReq["serviceAccount"] = d.Get("service_account").(string)
+	ec2CreateReq["purpose"] = d.Get("purpose").(string)
+	ec2CreateReq["dbsDbnameToRestore"] = d.Get("dbs_dbnameto_restore").(string)
+	ec2CreateReq["serverType"] = d.Get("server_type").(string)
+
+	// Convert to JSON.
+	ec2CreateJson, err := json.Marshal(ec2CreateReq)
+	log.Printf("Json Obtained %s", string(ec2CreateJson))
+
+	//endpoint := "https://1w3zqo2l9i.execute-api.us-east-1.amazonaws.com/default/python_lambda"
+	endpoint := "https://dv2-api.dbaas.aenetworks.com/aws/ec2/create"
+	endpoint_from_tf := d.Get("endpoint").(string)
+	if len(endpoint_from_tf) > 0 {
+		endpoint = endpoint_from_tf
+	}
+	log.Printf("endpoint %s", endpoint)
+	
+	// Create Request.
+	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(ec2CreateJson))
+	req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Content-Type", "application/json")
+	log.Printf("Request Created")
+
+	// AWS IAM AUTH.
 	awsAuth := v4.NewSigner(credentials.NewStaticCredentials(
-	    meta.(*dbaasCreds).access_key,
-	    meta.(*dbaasCreds).secret_key,
-			"",))
-	awsAuth.Sign(req, nil, "execute-api", "us-east-1", time.Now())
+		meta.(*dbaasCreds).access_key,
+		meta.(*dbaasCreds).secret_key,
+		""))
+	awsAuth.Sign(req, bytes.NewReader(ec2CreateJson), "execute-api", "us-east-1", time.Now())
+	log.Printf("Signed.")
+
 	client := &http.Client{}
-	log.Println(meta.(*dbaasCreds).access_key)
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal("Do: ", err)
-		//return
 	}
-	log.Printf("Response received from Num Verify API. \r\n")
 
-	// Callers should close resp.Body
-	// when done reading from it
-	// Defer the closing of the body
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyString := string(bodyBytes)
+	log.Printf("Response Received %s", bodyString)
+
+	var data CreateResponse
+
+	err = json.Unmarshal(bodyBytes, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer resp.Body.Close()
 
-	// Fill the record with the data from the JSON
-	var record Response
+	if data.StatusCode == 200 {
+		log.Printf("Decoded. %v %d", data.Message, data.ServiceRequesID)
 
-	// Use json.Decode for reading streams of JSON data
-	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
-		log.Println(err)
+		// Setting the ResourceID.
+		d.SetId(strconv.Itoa(data.ServiceRequesID))
+
+		// Setting the RequestID Out.
+		d.Set("service_request_id_out", strconv.Itoa(data.ServiceRequesID))
+		return nil
 	}
+	return errors.New(data.Message)
 
-	log.Printf(record.RequestIDOut)
-
-	//log.Printf("Mobile Number = %s \r\n", record.ID)
-	d.SetId(record.RequestIDOut)
-	d.Set("service_request_id_out", record.RequestIDOut)
-	return nil
 }
 
 func readEC2(d *schema.ResourceData, meta interface{}) error {
-	//hi := "heyy"
-
 	return nil
 }
 
@@ -187,5 +204,57 @@ func updateEC2(d *schema.ResourceData, meta interface{}) error {
 }
 
 func deleteEC2(d *schema.ResourceData, meta interface{}) error {
-	return nil
+	// Logging.
+	file, err := os.OpenFile("logs/ec2_destroy.log", os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	log.SetOutput(file)
+
+	// Create Request.
+	endpoint := fmt.Sprintf("https://dv2-api.dbaas.aenetworks.com/aws/ec2/delete?serviceRequestId=%s", d.Id())
+	req, err := http.NewRequest("DELETE", endpoint, nil)
+	req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Content-Type", "application/json")
+	log.Printf("Request Created %s", endpoint)
+
+	// AWS IAM AUTH.
+	awsAuth := v4.NewSigner(credentials.NewStaticCredentials(
+		meta.(*dbaasCreds).access_key,
+		meta.(*dbaasCreds).secret_key,
+		""))
+	awsAuth.Sign(req, nil, "execute-api", "us-east-1", time.Now())
+	log.Printf("Signed.")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+	}
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyString := string(bodyBytes)
+	log.Printf("Response Received %s", bodyString)
+
+	var data DeleteResponse
+
+	err = json.Unmarshal(bodyBytes, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if data.StatusCode == 200 {
+		log.Printf("Checking Decode JSON. %v", data.Message)
+		log.Printf("Service Request ID to delete: %s", d.Id())
+
+		// Deleting the resource.
+		d.SetId(" ")
+		return nil
+	}
+	return errors.New(data.Message)
 }
