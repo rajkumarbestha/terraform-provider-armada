b36af74a5af4db429996dda5ece3e7ab44dc965b
Rajkumar <rajkumarbestha97@gmail.com>
2019-08-27 15:39:40 +0530

    resource_rds_tf changes for devops

--- sample_tf_scripts/armada_rds.tf
+++ sample_tf_scripts/armada_rds.tf
@@ -1,25 +1,27 @@
 provider "armada"{
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = "" //required, Can be given at run time also.
+  secret_key = "" //required, Can be given at run time also.
 }
 
 resource "armada_rds" "dev_rds" {
-  database_engine = "oracle-ee"
-  application_code = "DOC"
-  environment = "UAT"
-  request_type_id = "5"
-  request_from = "1"
-  database_engine_verison = "12.1"
-  db_restore = "false"
-  db_restore_path = ""
-  database_name = "DOC"
-  allocated_storage = "100"
-  db_instance_class = "db.t2.medium"
-  request_created_date = "2019-08-26T09:35:41.707Z"
-  request_raised_by ="rajkumarb"
-  purpose = "Terraform Plugin Testing"
+  database_engine = "oracle-ee" //required
+  application_code = "DOC" //required
+  environment = "UAT" //required
+  request_type_id = "5" //required
+  request_from = "1" //required
+  database_engine_verison = "12.1" //required
+  db_restore = "false" //required
+  db_restore_path = "" //required
+  database_name = "DOC" //required
+  allocated_storage = "100" //required
+  db_instance_class = "db.t2.medium" //required
+  request_created_date = "2019-08-26T09:35:41.707Z" //required
+  request_raised_by ="rajkumarb@aenetworks.com" //required
+  purpose = "Terraform Plugin Testing" //required
+  endpoint = "" //required
 }
 
 output "rds_request_ID" {
 	value = ["${armada_rds.dev_rds.*.service_request_id_out}"]
+	// service_request_id_out is a computed variable
 }
