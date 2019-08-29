a0a6d2187610339a1adcdbb74d16c46e167dbd64
rajkumarbestha <rajkumarbestha>
2019-08-29 17:06:59 +0530

    armada package in go

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
+	"github.com/rajkumarbestha/terraform-provider-armada/armada"
 )
 
 func main() {
 	opts := plugin.ServeOpts{
-		ProviderFunc: armadaProvider,
+		ProviderFunc: armada.Provider,
 	}
 	plugin.Serve(&opts)
 }
